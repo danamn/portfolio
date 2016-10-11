200b5c14f976436d2f75a3539e63b3f52e8a2ddf
Dana Nica <dana.mn@gmail.com>
2016-10-11 11:50:12 -0400

    delete src folder

--- src/react/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Layout from './Resume-Components/Layout'
-
-const resumeDiv = document.getElementById('resume-container');
-
-ReactDOM.render( <Layout />, resumeDiv);
