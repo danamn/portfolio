c7e904cee70a2a39a99829d8a1405fc46fc12960
Dana Nica <dana.mn@gmail.com>
2019-01-26 19:56:49 -0500

    update app

---
+++ docs/projects/calculationsKids/precache-manifest.cbaa58f676766ee35f8899df5835725d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b6050a9eebb00ea5dfe2fe8b85a17a3",
+    "url": "/portfolio/projects/calculationsKids/static/media/sign2.9b6050a9.jpg"
+  },
+  {
+    "revision": "9940918494d9821268671f43190bb34b",
+    "url": "/portfolio/projects/calculationsKids/static/media/red-star-hi.99409184.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/projects/calculationsKids/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e0fb0c158865dce6f15bf76f7db4840",
+    "url": "/portfolio/projects/calculationsKids/static/media/grey-star-hi.1e0fb0c1.png"
+  },
+  {
+    "revision": "e7d7787e096258ba5eeab9cecfd6549f",
+    "url": "/portfolio/projects/calculationsKids/static/media/green-star-hi.e7d7787e.png"
+  },
+  {
+    "revision": "ea956543a64fdb6d9812",
+    "url": "/portfolio/projects/calculationsKids/static/js/runtime~main.ea956543.js"
+  },
+  {
+    "revision": "fe3dcbb29ca585cc1705",
+    "url": "/portfolio/projects/calculationsKids/static/js/main.fe3dcbb2.chunk.js"
+  },
+  {
+    "revision": "6d38667c0f850e6d87fb",
+    "url": "/portfolio/projects/calculationsKids/static/js/1.6d38667c.chunk.js"
+  },
+  {
+    "revision": "fe3dcbb29ca585cc1705",
+    "url": "/portfolio/projects/calculationsKids/static/css/main.c17d0473.chunk.css"
+  },
+  {
+    "revision": "fac3382172efdcea6c4d7aa3bb2b1774",
+    "url": "/portfolio/projects/calculationsKids/index.html"
+  }
+];
--- docs/projects/calculationsKids/service-worker.js
+++ docs/projects/calculationsKids/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projects/calculationsKids/precache-manifest.ec13a8e4c148b8978aba85890cecf9a6.js"
+  "/portfolio/projects/calculationsKids/precache-manifest.cbaa58f676766ee35f8899df5835725d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/projects/calculationsKids/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/projects/calculationsKids/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
